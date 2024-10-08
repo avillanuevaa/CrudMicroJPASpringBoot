9157df1e7b9f906d4df9d7ea7878fd16c3fa4cba
Anthony Villanueva Arrestegui (TopStrategic) <ava.topstrategic@usil.edu.pe>
2024-10-08 14:40:14 -0500

    chnague string conecction MongoConfig.java getting from application.properties

--- src/main/java/com/api/crud/config/MongoConfig.java
+++ src/main/java/com/api/crud/config/MongoConfig.java
@@ -1,20 +1,34 @@
 package com.api.crud.config;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.host}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private int mongoPort;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDatabase;
+
     @Bean
     public MongoClient mongoClient() {
-        return new MongoClient("localhost", 27017);
+        // Crear un URI de conexión basado en los valores del properties
+        String mongoUri = "mongodb://" + mongoHost + ":" + mongoPort;
+        return new MongoClient(new MongoClientURI(mongoUri));
     }
 
     @Bean
     public MongoDatabase mongoDatabase() {
-        return mongoClient().getDatabase("BdSabana");
+        // Usar el nombre de la base de datos del properties
+        return mongoClient().getDatabase(mongoDatabase);
     }
 }
